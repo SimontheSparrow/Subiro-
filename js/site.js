54f9680a79542059d44fe1a2c5e9aa7ccef3436f
SimontheSparrow <88796995+SimontheSparrow@users.noreply.github.com>
2021-12-19 09:59:40 +0100

    Done

--- js/site.js
+++ js/site.js
@@ -1,3 +1,45 @@
-function HelloWorld() {
-    alert("Hello world");
+//get values
+function getValues() {
+
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+        let numbers = generateNumbers(startValue, endValue);
+        displayNumbers(numbers);
+    } else {
+        alert("Enter integers");
+    }
 }
+
+// generate numbers
+function generateNumbers(sValue, eValue) {
+
+    let numbers = [];
+
+    for (let i = sValue; i <= eValue; i++) {
+        numbers.push(i);
+
+    }
+    return numbers;
+}
+
+// display numbers and make even bold
+function displayNumbers(numbers) {
+    let templateRows = "";
+    for (let index = 0; index < numbers.length; index++) {
+        let number = numbers[index];
+        if (number % 2 === 0) {
+            templateRows += `<tr><td><strong>${number}</strong></td></tr>`;
+        } else {
+            templateRows += `<tr><td>${number}</td></tr>`;
+        }
+
+        document.getElementById("results").innerHTML = templateRows;
+
+    }
+}
+
